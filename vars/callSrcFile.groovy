d4b6c794ac2b5554d0e78706ab870bf966cb1403
Rajeshkumar <rajeshkumar27390@gmail.com>
2020-09-23 21:39:11 +0530

    Update callSrcFile.groovy

--- vars/callSrcFile.groovy
+++ vars/callSrcFile.groovy
@@ -3,3 +3,10 @@ def work(int x,int y){
     def tes=myUtils.add(x,y)
     return tes
 }
+def check(Map pipelineParams)
+{
+println "Checkout Git"
+ myUtils = new org.validAdd()
+myUtils.checkout(pipelineParams.branchName, pipelineParams.giturl)
+//git branch: pipelineParams.branchName, url: pipelineParams.giturl
+}
