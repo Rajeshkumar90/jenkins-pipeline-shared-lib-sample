ae058f8a43557679bbad2c11138876836903793b
Rajeshkumar <rajeshkumar27390@gmail.com>
2020-09-23 15:02:31 +0530

    Update testStep.groovy

--- vars/testStep.groovy
+++ vars/testStep.groovy
@@ -3,3 +3,13 @@ def checkout(Map pipelineParams)
 println "Checkout Git"
   git branch: pipelineParams.branchName, url: pipelineParams.giturl
 }
+def call(body) {
+    echo "Start Deploy"
+
+    new Sample(script:this).run()
+
+    echo "Deployed"
+    currentBuild.result = 'SUCCESS' //FAILURE to fail
+
+    return this
+}
