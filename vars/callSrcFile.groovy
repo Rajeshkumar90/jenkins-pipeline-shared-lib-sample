1f675e6d0f814eb13d6fd3c85c4f1909d50cda43
Rajeshkumar <rajeshkumar27390@gmail.com>
2020-09-24 12:13:09 +0530

    Update callSrcFile.groovy

--- vars/callSrcFile.groovy
+++ vars/callSrcFile.groovy
@@ -1,14 +1,15 @@
 package org
 import org.*
-def myUtils = new org.validAdd()
-def work(int x,int y){    
+
+def work(int x,int y){ 
+    def myUtils = new org.validAdd()
     def tes=myUtils.add(x,y)
     return tes
 }
 def check(Map pipelineParams)
 {
 println "Checkout Git"
- //myUtils = new org.validAdd()
+myUtils = new org.validAdd()
 myUtils.checkout(pipelineParams.branchName, pipelineParams.giturl)
 //git branch: pipelineParams.branchName, url: pipelineParams.giturl
 }
