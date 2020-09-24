42c6a157cee67d6c7dbb49911c8e8f2aed804df3
Rajeshkumar <rajeshkumar27390@gmail.com>
2020-09-24 13:05:16 +0530

    Update pipelineinVarFolder.groovy

--- vars/pipelineinVarFolder.groovy
+++ vars/pipelineinVarFolder.groovy
@@ -1,4 +1,30 @@
-def call(Map config)
-{
-println "Pipeline"
+def call(Map pipelineParams) {
+def myUtils = new org.validAdd()
+    pipeline {
+        agent {
+		label "master"
+		}
+	environment{
+		credID = credentials("3dcb170c-4677-411a-9720-bc20898f1b07")
+		}		
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    script {
+                     println "Checkout Git"
+						//myUtils = new org.validAdd()
+						myUtils.checkout(this,pipelineParams.branchName, pipelineParams.giturl)
+                    }
+                }
+            }
+            stage('Addition') {
+                steps {
+                    script {
+                        def tes=myUtils.add(pipelineParams.x,pipelineParams.y)
+						          println "Returned :"+tes 
+                    }
+                }
+            }
+        }
+    }
 }
