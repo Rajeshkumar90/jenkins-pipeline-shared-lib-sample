941b1ae1462680c7c76181d2b393544e14e93302
Rajeshkumar <rajeshkumar27390@gmail.com>
2020-09-24 12:12:22 +0530

    Update validAdd.groovy

--- src/org/validAdd.groovy
+++ src/org/validAdd.groovy
@@ -1,15 +1,16 @@
 package org
 
-
+class validAdd{
 def add(int x,int y)
 {
 int z= x+y
 return z
+}
 }
 def checkout(String branchName,String giturl)
 {
 println "Checkout Git"
 git branch: branchName, url: giturl
 }
-
+return this
 
